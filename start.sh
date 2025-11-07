4024f64f5fef72753848a30d271a72f12c0120b7
Angel Vilar Sánchez <angelvs@MacBook-Pro-de-Angel.local>
2025-11-07 11:50:37 +0100

    Corrige orden de parámetros en start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 echo "[start.sh] Iniciando Rasa en PORT=${PORT:-5005} ..."
-exec rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} --credentials credentials.yml
+exec rasa run \
+  --enable-api \
+  --cors "*" \
+  --port ${PORT:-5005} \
+  --host 0.0.0.0 \
+  --credentials credentials.yml
+
