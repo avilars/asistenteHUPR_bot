0ffba3534880db03059f7d9cdcb6e8899772c83b
avilars <avilars@hotmail.com>
2025-11-07 11:13:01 +0100

    Update Rasa startup command in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "[start.sh] Iniciando Rasa con PORT=${PORT:-5005} ..."
-exec python3 -m rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models --credentials credentials.yml
+echo "[start.sh] Iniciando Rasa en PORT=${PORT:-5005} ..."
+exec rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} --credentials credentials.yml
